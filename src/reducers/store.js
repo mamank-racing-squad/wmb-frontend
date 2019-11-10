7944e8890e334c3ed403b5c4d4938ce7b0d2d027
Tn Martin Jham <academyyyy12@gmail.com>
2019-11-10 16:24:40 +0700

    fix bug

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,22 +1,29 @@
-import {combineReducers} from 'redux'
+import { combineReducers } from 'redux'
 
 function dealSelectedItem(state = [], action) {
-
+    
     switch (action.type) {
         case 'ADD_SELECTED_ITEM':
-            return [action.payload, ...state];
+            {
+                return [action.payload, ...state]
+            }
         case 'DEL_SELECTED_ITEM':
-            return state.filter(selectedItem => selectedItem.name !== action.payload.name);
+            {
+                return state.filter(selectedItem => selectedItem.name !== action.payload.name)
+            }
         case 'UPDATE_QUANTITY':
-            return state.map((item) => {
-                if (action.payload.name === item.name) {
-                    return Object.assign({}, item, {quantity: action.payload.quantity})
-                }
-                return item
-            });
-        case 'CLEAR_SELECTED_ITEMS': {
-            return []
-        }
+            {
+                return state.map((item) => {
+                    if(action.payload.name == item.name) {
+                        return Object.assign({}, item, {quantity: action.payload.quantity})
+                    }
+                    return item
+                })
+            }
+            case 'CLEAR_SELECTED_ITEMS':
+            {
+                return []
+            }
         default:
             return state
     }
@@ -25,22 +32,29 @@ function dealSelectedItem(state = [], action) {
 function dealFoods(state = [], action) {
     switch (action.type) {
         case 'ADD_FOODS':
-            return action.payload;
+            {
+                return action.payload
+            }
         case 'UPDATE_FOOD':
-            return action.payload;
+            {
+                return action.payload
+            }
         case 'UPDATE_SELECTED_FOOD':
-            state.map((item) => {
-                if (action.payload.name === item.name)
-                    item.isSelected = action.payload.isSelected;
+            {
+                state.map((item) => {
+                    if(action.payload.name == item.name)
+                    item.isSelected = action.payload.isSelected
 
-                return Object.assign(state.filter(foods => foods.name !== action.payload.name), item)
-            });
-            break;
+                    return Object.assign(state.filter(foods => foods.name !== action.payload.name), item)
+                })
+            }
         case 'SET_DEFAULT_FOODS':
-            return state.map((item) => {
-                item.isSelected = false;
-                return item
-            });
+            {
+                return state.map((item) => {
+                    item.isSelected = false
+                    return item
+                })
+            }
         default:
             return state
     }
@@ -49,21 +63,29 @@ function dealFoods(state = [], action) {
 function dealDrinks(state = [], action) {
     switch (action.type) {
         case 'ADD_DRINKS':
-            return action.payload;
+            {
+                return action.payload
+            }
         case 'UPDATE_DRINK':
-            return action.payload;
+        {
+            return action.payload
+        }
         case 'UPDATE_SELECTED_DRINK':
-            state.map((item) => {
-                if (action.payload.name === item.name)
-                    item.isSelected = action.payload.isSelected;
-                return Object.assign(state.filter(drinks => drinks.name !== action.payload.name), item)
-            });
-            break;
+            {
+                state.map((item) => {
+                    if(action.payload.name == item.name)
+                    item.isSelected = action.payload.isSelected
+
+                    return Object.assign(state.filter(drinks => drinks.name !== action.payload.name), item)
+                })
+            }
         case 'SET_DEFAULT_DRINKS':
-            return state.map((item) => {
-                item.isSelected = false;
-                return item
-            });
+            {
+                return state.map((item) => {
+                    item.isSelected = false
+                    return item
+                })
+            }
         default:
             return state
     }
@@ -73,6 +95,6 @@ const itemApp = combineReducers({
     selectedItem: dealSelectedItem,
     foods: dealFoods,
     drinks: dealDrinks
-});
+})
 
 export default itemApp
